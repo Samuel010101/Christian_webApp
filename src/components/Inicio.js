66b0a817d6a1a31e64591becd867d9f0ac5e64d9
Sam Minyety <samuelmartinez11591.sm@gmail.com>
2021-02-26 16:45:11 -0400

    Se agrego la libreria bigCalendar

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Carousel from "semantic-ui-carousel-react";
-import { Image, Button } from "semantic-ui-react";
+import { Image } from "semantic-ui-react";
 //import "semantic-ui-css/semantic.min.css";
 
 
