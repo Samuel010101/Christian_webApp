386f07fb7a544dbd206bec6d16839a94ac29b593
Sam Minyety <samuelmartinez11591.sm@gmail.com>
2021-03-01 14:37:40 -0400

    Cambios en el componente inicio y componente iglesia de Dios; se agrego Ant Design

--- src/components/IglesiaDeDios.js
+++ src/components/IglesiaDeDios.js
@@ -1,17 +1,63 @@
-import React from 'react';
+import React from "react";
+import Carousel from "semantic-ui-carousel-react";
+import { Image } from "semantic-ui-react";
+//import "semantic-ui-css/semantic.min.css";
+
+import corazon from '../img/corazon.jpeg';
+import biblia from '../img/download.jpeg';
+import gota from '../img/gota.png';
+import imagen1 from '../img/imagen_1.jpeg';
+import imagen2 from '../img/imagen_2.jpeg';
 
 
 const IglesiaDeDios = () => {
-
-    return (
-        <div>
-        <div className="ui form">
-            <div className="field">
-                <label>Iglesia De Dios</label>
-            </div>
-        </div>
+  let elements = [
+    {
+      render: () => {
+        return (
+          <Image className="slider" src={corazon} alt="Imagen 1" />
+        );
+      }
+    },
+    {
+      render: () => {
+        return (
+        <Image className="slider" src={biblia} alt="Biblia" />
+        );
+      }
+    },
+    {
+      render: () => {
+        return (
+          <Image className="slider" src={gota} alt="Gota" />
+        );
+      }
+    },
+    {
+      render: () => {
+        return (
+          <Image className="slider" src={imagen1} alt="Culto de jovenes" />
+        );
+      }
+    },
+    {
+      render: () => {
+        return (
+          <Image className="slider" src={imagen2} alt="Tiempo de afilamiento" />
+        );
+      }
+    }
+  ];
+  return (
+    <div>
+      <Carousel
+        elements={elements}
+        duration={10000}
+        animation="slide right"
+        showNextPrev={false}
+        showIndicators={true}
+      />
     </div>
-    );
+  );
 };
-
 export default IglesiaDeDios;
--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,3 +1,92 @@
+import React, { useState } from 'react';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Layout, Menu } from 'antd';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
+
+const { Header, Sider, Content } = Layout;
+
+const Inicio = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    
+
+  const toggle = () => {
+    setCollapsed({
+      collapsed: collapsed
+    });
+  };
+
+  
+    return (
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              nav 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              nav 2
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              nav 3
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: collapsed.toggle,
+            })}
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            Content
+            <p>
+              La iglesia de Dios ha sido ordenada de forma imperativa a llevar a cabo la gran comision
+              del evangenlio de cristo
+            </p>
+          </Content>
+        </Layout>
+      </Layout>
+    );
+  
+}
+
+export default Inicio;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
 import React from "react";
 import Carousel from "semantic-ui-carousel-react";
 import { Image } from "semantic-ui-react";
@@ -49,7 +138,7 @@ const Inicio = () => {
     }
   ];
   return (
-    <div  /*style={{ width: 500}}*/ >
+    <div>
       <Carousel
         elements={elements}
         duration={10000}
@@ -66,3 +155,4 @@ export default Inicio;
 
 
 
+ */
